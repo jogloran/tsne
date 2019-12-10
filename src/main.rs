08f69042fc0e7397e47c51ce468434885f59754b
Daniel Tse <daniel@overpunch.com>
2019-12-10 00:56:48 -0800

    update

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,25 @@ extern crate rand;
 use core::borrow::Borrow;
 use std::iter::*;
 
-use itertools::Itertools;
 use minifb::*;
 use mnist::{Mnist, MnistBuilder};
-use ndarray::{s, Shape};
+use ndarray::s;
 use ndarray::Array;
-use ndarray::linalg::Dot;
 use ndarray::prelude::*;
 use ndarray_rand::RandomExt;
-use rand::distributions::{Normal, Range};
+use rand::distributions::Normal;
 use libc;
 use std::mem;
 use std::collections::HashMap;
 use std::f64;
 
-const MAX_PERP_SEARCH_ITERS: u32 = 20;
-const PERP: f64 = 10.0;
-const LR: f64 = 10f64;
+const MAX_PERP_SEARCH_ITERS: u32 = 50;
+const PERP: f64 = 20.0;
+const LR: f64 = 30f64;
 const MAX_SGD_ITERS: i32 = 1000;
-const WIDTH: usize = 1024;
+const WIDTH: usize = 768;
 const HEIGHT: usize = 768;
+const TRN_SIZE: usize = 400;
 
 type Data = Array2<f64>;
 type DistanceMatrix = Array2<f64>; // (ndatum, ndatum)
@@ -35,43 +34,45 @@ type DistanceMatrix = Array2<f64>; // (ndatum, ndatum)
 fn distances(data: &Data) -> DistanceMatrix {
     let n: usize = data.shape()[0];
     Array2::from_shape_fn((n, n), |(i, j)| {
-        let xi = data.slice(s![i, ..]);
-        let xj = data.slice(s![j, ..]);
-        norm_sq(&xi, &xj)
+        if i == j {
+            0.
+        } else {
+            let xi = data.slice(s![i, ..]);
+            let xj = data.slice(s![j, ..]);
+            norm_sq(&xi, &xj)
+        }
     })
 }
 
 fn conditional_dist(dists: &DistanceMatrix, i: usize, sigma_i: f64) -> Array1<f64> {
     let n = dists.shape()[0] as usize;
     let d = dists.slice(s![i, ..]);
-    let mut all = Array1::from_shape_fn(n, |j| {
+    let all = Array1::from_shape_fn(n, |j| {
         if i == j {
             0.
         } else {
             (-d[j] / (2. * sigma_i * sigma_i)).exp()
         }
     });
-    all /= all.sum();
-    all
+    &all / all.sum()
 }
 
 fn joint_t_dist(dists: &DistanceMatrix) -> Array2<f64> {
     let n = dists.shape()[0] as usize;
-    let mut all = Array2::from_shape_fn((n, n), |(k, l)| {
+    let all = Array2::from_shape_fn((n, n), |(k, l)| {
         if k == l {
             0.
         } else {
             1.0 / (1.0 + dists[[k, l]])
         }
     });
-    let denom = all.sum();
-    all / denom
+    &all / all.sum()
 }
 
-fn perp_search(dists: &DistanceMatrix, data: &Data, i: usize, max_iters: u32, target_perp: f64) ->
+fn perp_search(dists: &DistanceMatrix, i: usize, max_iters: u32, target_perp: f64) ->
 (Array1<f64>, f64) {
-    let mut s_min = 0.001f64;
-    let mut s_max = 100f64;
+    let mut s_min = 0.000001f64;
+    let mut s_max = 1000f64;
     let mut s = 1.0;
     let mut iter = 0;
     let mut dist = conditional_dist(&dists, i, s);
@@ -86,6 +87,7 @@ fn perp_search(dists: &DistanceMatrix, data: &Data, i: usize, max_iters: u32, ta
             s_min = s;
         }
         s = s_min + (s_max - s_min) / 2.;
+//        s = (s_max + s_min) / 2.0;
 
         iter += 1;
 
@@ -100,53 +102,40 @@ fn perp_search(dists: &DistanceMatrix, data: &Data, i: usize, max_iters: u32, ta
     (dist, s)
 }
 
-fn conditional_dist_search(dists: &DistanceMatrix, data: &Data, target_perp: f64) -> Array2<f64> {
+fn symmetrised_dist_search(dists: &DistanceMatrix, target_perp: f64) -> Array2<f64> {
     let n = dists.shape()[0] as usize;
 
     let mut result = Array2::zeros((n, n));
     for i in 0..n {
-        let (p_ji, s_i) = perp_search(&dists, &data, i, MAX_PERP_SEARCH_ITERS, target_perp);
+        let (p_ji, _s_i) = perp_search(&dists, i, MAX_PERP_SEARCH_ITERS, target_perp);
         result.row_mut(i).assign(&p_ji);
     }
 
-    result
+    (&result + &result.t()) / (2.0f64 * (n as f64))
 }
 
-fn symmetrised_dist_search(dists: &DistanceMatrix, data: &Data, target_perp: f64) -> Array2<f64> {
-    // for each datum x_i,
-    // i is fixed
-    let n = dists.shape()[0] as usize;
-
-    let mut result = Array2::zeros((n, n));
-    for i in 0..n {
-        let (p_ji, s_i) = perp_search(&dists, &data, i, MAX_PERP_SEARCH_ITERS, target_perp);
-        result.row_mut(i).assign(&p_ji);
-    }
-    let mut result2 = Array2::zeros((n, n));
-    for j in 0..n {
-        let (p_ji, s_i) = perp_search(&dists, &data, j, MAX_PERP_SEARCH_ITERS, target_perp);
-        result2.row_mut(j).assign(&p_ji);
-    }
-
-    let mut symmetrised = Array2::zeros((n, n));
-    symmetrised = (result + result2) / (2.0f64 * (n as f64));
-    symmetrised
-}
-
-fn H(dist: &Array1<f64>) -> f64 {
-    let ref log_dist = dist.mapv(|x: f64| (x + 1e-8).log2());
+fn entropy(dist: &Array1<f64>) -> f64 {
+    let ref log_dist = dist.mapv(|x: f64|
+        if x < 1e-8 {
+            0.
+        } else {
+            x.log2()
+        }
+    );
     -(dist * log_dist).sum()
 }
 
 fn perp(dist: &Array1<f64>) -> f64 {
-    2.0_f64.powf(H(&dist))
+    2.0_f64.powf(entropy(&dist))
 }
 
-fn grad(proj: &Array2<f64>, p_ij: &Array2<f64>, q_ij: &Array2<f64>)
+fn grad(proj: &Array2<f64>, p_ij: &Array2<f64>, q_ij: &Array2<f64>,
+        lo_dists: &Array2<f64>)
         -> Array2<f64> {
     let n = proj.shape()[0] as usize;
     // result is same size as proj
     let mut result = Array::zeros(proj.raw_dim());
+    let p_minus_q = p_ij.borrow() - q_ij.borrow();
 
     for i in 0..n {
         let mut sum = Array1::zeros(2);
@@ -154,23 +143,14 @@ fn grad(proj: &Array2<f64>, p_ij: &Array2<f64>, q_ij: &Array2<f64>)
             if i == j {
                 continue;
             }
-//            println!("1 {}", &proj.row(i));
-//            println!("2 {}", &proj.row(j));
-//            println!("a {:?}", &proj.row(i) - &proj.row(j));
-//            println!("b {}", norm_sq(&proj.row(i), &proj.row(j)));
-//            println!("c {}", 4. * (p_ij[[i, j]] - q_ij[[i, j]])
-//                * (proj.row(i).borrow() - proj.row(j).borrow())
-//                / (1. + norm_sq(&proj.row(i), &proj.row(j))));
-            sum = sum + (p_ij[[i, j]] - q_ij[[i, j]])
+
+            sum = sum + (p_minus_q[[i, j]])
                 * (proj.row(i).borrow() - proj.row(j).borrow())
-                / (1. + norm_sq(&proj.row(i), &proj.row(j)));
+                / (1. + lo_dists[[i, j]]);
         }
         sum *= 4.0;
-//        println!("sum {}", sum);
-
         result.row_mut(i).assign(&sum);
     }
-
     result
 }
 
@@ -237,8 +217,7 @@ fn update_proj(buf: &mut Vec<u32>, proj: &Array2<f64>, lbls: &Vec<u8>) {
 }
 
 fn main() {
-    let (trn_size, rows, cols) = (300, 28, 28);
-    let size: i32 = trn_size * rows * cols;
+    let (trn_size, rows, cols) = (TRN_SIZE, 28, 28);
 
     let Mnist { trn_img, trn_lbl, .. } = MnistBuilder::new()
         .label_format_digit()
@@ -246,15 +225,15 @@ fn main() {
         .validation_set_length(100)
         .test_set_length(100)
         .finalize();
-    println!("lbl {:?}", trn_lbl);
+    let mut lbl = trn_lbl.clone();
+    lbl.sort();
+    println!("lbl {:?}", lbl);
 
-    let mut u8_images = Array::from_vec(trn_img)
+    let u8_images = Array::from_vec(trn_img)
         .into_shape((trn_size as usize, (rows * cols) as usize)).unwrap();
-    let mut images = u8_images.mapv(|f| {
+    let images = u8_images.mapv(|f| {
         (f as f64) / 255.
     });
-
-    //    println!("img {:?}", images.slice(s![1, ..]));
 
     let mut buf: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut window = match Window::new("Test", WIDTH, HEIGHT, WindowOptions::default()) {
@@ -265,29 +244,23 @@ fn main() {
         }
     };
 
-    let mut it = 0;
     let normal = Normal::new(0., 1e-4);
     let mut proj = Array::random((trn_size as usize, 2), normal);
     let mut velocity_prev: Array2<f64> = Array2::zeros((trn_size as usize, 2));
-    for i in 0..MAX_SGD_ITERS {
-        let mut dists = distances(&images);
-        let mut p_ij = symmetrised_dist_search(&dists, &images, PERP);
+    let dists = distances(&images);
+    let p_ij = symmetrised_dist_search(&dists, PERP);
+
+    for _ in 0..MAX_SGD_ITERS {
         let lo_dists = distances(&proj);
         let q_ij = joint_t_dist(&lo_dists);
-//    let mut proj = Array2::zeros((trn_size as usize, 2));
-//        println!("proj: {}", proj);
 
-        let del: Array2<f64> = grad(&proj, &p_ij, &q_ij);
-//        println!("p_ij: {}", p_ij);
-//        println!("q_ij: {}", q_ij);
-//        println!("{} del: {}", i, del);
+        let del: Array2<f64> = grad(&proj, &p_ij, &q_ij, &lo_dists);
         let velocity = LR * &del + 0.9 * &velocity_prev;
         velocity_prev.assign(&velocity);
         proj = proj - velocity;
 
-        let avg = proj.sum() / (proj.len() as f64);
-        proj -= avg;
-//        println!("{} proj: {}", i, proj);
+        let avg = proj.sum_axis(Axis(0)) / proj.len() as f64;
+        proj = proj - avg;
 
         update_proj(&mut buf, &proj, &trn_lbl);
         window.update_with_buffer_size(&buf, WIDTH, HEIGHT).unwrap();
